59e183741c6901cc79e3e8b155dae62764d0a0e5
Aleksey <11_alx_11@ro.ru>
2024-03-17 21:15:23 +0300

    work on labrab2

--- labrab2/app/src/main/java/com/RPOMP/labrab2/MainActivity.java
+++ labrab2/app/src/main/java/com/RPOMP/labrab2/MainActivity.java
@@ -2,7 +2,9 @@ package com.RPOMP.labrab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AccelerometerActivity.class);
+            startActivity(intent);
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GPSActivity.class);
+            startActivity(intent);
+        });
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SputnikActivity.class);
+            startActivity(intent);
+        });
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, OthersActivity.class);
+            startActivity(intent);
+        });
     }
 }
